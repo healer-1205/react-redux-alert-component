cd2a6359429c50cb9fc620cfd2bff9487b549af1
healer-1205 <crazyhealer1205@gmail.com>
2022-08-09 09:27:29 -0400

    add input box

--- src/components/AlertComponent.js
+++ src/components/AlertComponent.js
@@ -1,53 +1,43 @@
-import * as React from "react";
+import React from "react";
+import { useState } from "react";
 import Alert from "@mui/material/Alert";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
+import TextField from '@mui/material/TextField';
 import { useAlertReducer } from "../AlertManager";
 
 function AlertComponent() {
-	const { addAlert, state } = useAlertReducer()
+	const { addAlert, state } = useAlertReducer();
+	const [alerts, setAlerts] = useState({
+		alerttype: "",
+		text: "",
+		color: "",
+		timelimit: 0,
+	})
+
+	const handleChange = (e) => {
+		setAlerts({
+			...alerts, [e.target.name]: e.target.value
+		});
+		console.log(alerts);
+	}
+
 	return (
 		<>
 			<Stack direction="row" spacing={2} sx={{ width: "100%" }} mt={5}>
-				<Button
-					variant="contained"
-					color="success"
-					onClick={(e) => {
-						e.preventDefault();
-						addAlert({ alertType: "success", text: "Success", timeLimit: 10000, color: "success" });
-					}}
-				>
-					ADD SUCCESS ALERT
-				</Button>
-				<Button
-					variant="contained"
-					color="warning"
-					onClick={(e) => {
-						e.preventDefault();
-						addAlert({ alertType: "warning", text: "Warning", timeLimit: 10000, color: "warning" });
-					}}
-				>
-					ADD WARNING ALERT
-				</Button>
-				<Button
-					variant="contained"
-					color="info"
-					onClick={(e) => {
-						e.preventDefault();
-						addAlert({ alertType: "info", text: "Info", timeLimit: 10000, color: "info" });
-					}}
-				>
-					ADD INFO ALERT
-				</Button>
+				<TextField id="outlined-basic" name="text" label="Text" variant="outlined" value={alerts.text} onChange={handleChange} />
+				<TextField id="outlined-basic" name="color" label="Color" variant="outlined" value={alerts.color} onChange={handleChange} />
+				<TextField type="number" id="outlined-basic" name="timelimit" label="TimeLimit" variant="outlined" value={alerts.timelimit} onChange={handleChange} />
+				<TextField id="outlined-basic" name="alerttype" label="AlertType" variant="outlined" value={alerts.alerttype} onChange={handleChange} />
 				<Button
 					variant="contained"
 					color="error"
 					onClick={(e) => {
 						e.preventDefault();
-						addAlert({ alertType: "error", text: "Error", timeLimit: 10000, color: "error" });
+						addAlert({ alertType: alerts.alerttype, text: alerts.text, timeLimit: alerts.timelimit, color: alerts.color });
 					}}
 				>
-					ADD ERROR ALERT
+					GENERATE ALERT
 				</Button>
 			</Stack>
 			<Stack sx={{ width: "10%", position: "fixed", top: 0, right: 0 }} spacing={2} mt={2}>
